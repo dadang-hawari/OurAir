6275b74b9367df4283de30abe0794a27b20bcd8c
dadang-hawari <muhammaddadanghawari@gmail.com>
2024-05-21 20:13:43 +0700

    feat(template) add initial React template

---
+++ ourair/src/Route.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function Route() {
+  return (
+    <>
+      <h1>Halo</h1>
+    </>
+  )
+}
+
+export default Route
---
+++ ourair/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Route from './Route.jsx'
+import './styles/index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Route />
+  </React.StrictMode>,
+)
