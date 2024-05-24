000df45f3f21bd35199ef923b0d1ad76ae577ad4
Andrysm06 <andrysetiawanm2206@gmail.com>
2024-05-24 10:18:24 +0700

    update

--- src/Route.jsx
+++ src/Route.jsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Beranda from "./pages/Beranda";
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
+import Register from "./pages/Register";
 
-function Route() {
-  return (
-    <>
-      <h1>Halo</h1>
-    </>
-  )
+export default function Route() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Beranda />,
+    },
+    {
+      path: "/Login",
+      element: <Login />,
+    },
+
+    {
+      path: "/ForgotPassword",
+      element: <ForgotPassword />,
+    },
+    {
+      path: "/Register",
+      element: <Register />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
-
-export default Route
---
+++ src/pages/Beranda.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+import { Link } from "react-router-dom";
+
+export default function Beranda() {
+  return (
+    <div>
+      <button>
+        <Link to="/Login" className="text-black mt-2 hover:text-blue-500">
+          Login
+        </Link>
+      </button>
+    </div>
+  );
+}
