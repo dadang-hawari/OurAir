58ef000b0188bb22c62c6e2836c0ac19dac9fce8
dadang-hawari <muhammaddadanghawari@gmail.com>
2024-06-05 21:03:37 +0700

    add destinasi favorit

--- src/pages/Beranda.jsx
+++ src/pages/Beranda.jsx
@@ -2,10 +2,13 @@ import { Link, useLocation, useNavigate } from 'react-router-dom'
 import Header from '../components/Beranda/Header'
 import Toast from '../components/common/Toast'
 import Navbar from '../components/Navbar'
+import ButtonPrimary from '../components/ButtonPrimary'
 import { useEffect } from 'react'
 import { checkLocationState } from '../utils/checkLocationState'
 import { Bounce } from 'react-toastify'
 import Footer from './Footer'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export default function Beranda() {
   const navigate = useNavigate()
@@ -19,14 +22,14 @@ export default function Beranda() {
     <div>
       <Navbar />
       <Header />
-      <div className="w-full h-screen flex items-center justify-center">
-        <div className="relative">
-          <button>
-            <Link to="/Login" className="text-black hover:text-blue-500">
-              Login
-            </Link>
-          </button>
-        </div>
+
+      {/* Destinasi Favorit */}
+      <div className="mt-64 max-w-5xl mx-auto px-4">
+        <h2 className="font-bold ">Destinasi Favorit</h2>
+        <button className="text-sm text-white max-w-32 w-full h-12 rounded-xl bg-secondary">
+          <FontAwesomeIcon icon={faSearch} className="mr-2" />
+          Semua
+        </button>
       </div>
       <Toast
         autoClose={3000}
