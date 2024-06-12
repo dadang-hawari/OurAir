de83454e3d442430a747f58dc71882f7adc9dc80
dadang-hawari <muhammaddadanghawari@gmail.com>
2024-06-12 08:19:54 +0700

    add tentang

--- src/pages/RiwayatPemesanan.jsx
+++ src/pages/RiwayatPemesanan.jsx
@@ -1,6 +1,7 @@
 import {
   faArrowLeft,
   faChevronRight,
+  faIcons,
   faLineChart,
   faLocationDot,
   faLocationPin,
@@ -9,6 +10,29 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
 
 export default function RiwayatPemesanan() {
+  const DirectionArrow = () => {
+    return (
+      <svg
+        viewBox="0 0 164 6"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className=" w-[98.9%] absolute top-[-5px] "
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M164 3C164 3.27614 163.776 3.5 163.5 3.5H0V2.5H163.5C163.776 2.5 164 2.72386 164 3Z"
+          fill="#151515"
+        />
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M161.825 0.641565C161.636 0.452812 161.33 0.452812 161.142 0.641565C160.953 0.830318 160.953 1.13635 161.142 1.3251L162.8 2.98333L161.142 4.64157C160.953 4.83032 160.953 5.13635 161.142 5.3251C161.33 5.51386 161.636 5.51386 161.825 5.3251L163.825 3.3251C164.014 3.13635 164.014 2.83032 163.825 2.64157L161.825 0.641565Z"
+          fill="#151515"
+        />
+      </svg>
+    )
+  }
   return (
     <div className="max-w-5xl px-5 mx-auto">
       <h1 className="text-xl font-bold">Cari Penerbangan</h1>
@@ -23,7 +47,7 @@ export default function RiwayatPemesanan() {
         {/* Card */}
         {/* List Booking */}
         <div className="rounded-xl border w-full p-4">
-          <span className="bg-[#73CA5C] py-2 px-4 text-white rounded-full">Issued</span>
+          <span className="bg-[#73CA5C] py-1 px-3 text-white rounded-full">Issued</span>
           <div className="flex gap-x-5 my-4 justify-between w-full md:max-w-[468px] items-center">
             <div className="flex gap-x-2 justify-center w-fit">
               <FontAwesomeIcon icon={faLocationDot} className="text-gray-primary pt-1" />
@@ -38,25 +62,7 @@ export default function RiwayatPemesanan() {
             <div className="w-full text-center -mt-4">
               <h2 className="text-gray-600 text-xs pb-1 font-[600]">4h 0m</h2>
               <div className="w-full relative">
-                <svg
-                  viewBox="0 0 164 6"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  className=" w-[98.9%] absolute top-[-5px] "
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M164 3C164 3.27614 163.776 3.5 163.5 3.5H0V2.5H163.5C163.776 2.5 164 2.72386 164 3Z"
-                    fill="#151515"
-                  />
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M161.825 0.641565C161.636 0.452812 161.33 0.452812 161.142 0.641565C160.953 0.830318 160.953 1.13635 161.142 1.3251L162.8 2.98333L161.142 4.64157C160.953 4.83032 160.953 5.13635 161.142 5.3251C161.33 5.51386 161.636 5.51386 161.825 5.3251L163.825 3.3251C164.014 3.13635 164.014 2.83032 163.825 2.64157L161.825 0.641565Z"
-                    fill="#151515"
-                  />
-                </svg>
+                <DirectionArrow />
               </div>
             </div>
             <div className="flex gap-x-2 justify-center w-fit">
@@ -85,15 +91,40 @@ export default function RiwayatPemesanan() {
         </div>
 
         {/* Details */}
-        <div className="w-full max-w-[376px] bg-red-500">
+        <div className="w-full md:max-w-[376px]">
           <div className="flex justify-between">
             <h2 className="font-bold">Detail Pesanan</h2>
-            <span className="bg-[#73CA5C] py-2 px-4 text-white rounded-full">Issued</span>
+            <span className="bg-[#73CA5C] py-1 px-3 text-white rounded-full">Issued</span>
           </div>
-          <div>
-            Booking Code: <span className="text-accent">6723y2GH3</span>
+          <div className="my-2">
+            Booking Code: <span className="text-accent font-bold">6723y2GH3</span>
           </div>
-          <div></div>
+          <div className="flex justify-between">
+            <b className="font-bold">19:10</b>
+            <b className="text-soft-blue text-xs">Keberangkatan</b>
+          </div>
+          <p className="my-1">5 Maret 2023</p>
+          <b className="font-[600]">Soekarno Hatta - Terminal 1A Domestik</b>
+          <div className="text-sm">
+            <hr className="w-11/12 mx-auto my-3" />
+            <div className="flex items-center gap-x-2">
+              <FontAwesomeIcon icon={faIcons} className="text-yellow-400" />
+              <div className="w-full">
+                <div className="font-bold">
+                  <h4>Jet Air - Economy</h4>
+                  <h5>JT- 203</h5>
+                </div>
+                <div>
+                  <b>Informasi:</b>
+                  <p className="text-accent font-[600]">Penumpang 1: Mr. Harr</p>
+                  <p>ID: 12344</p>
+                  <p className="text-accent font-[600]">Penumpang 2: Miss her</p>
+                  <p>ID: 12341243423</p>
+                </div>
+              </div>
+            </div>
+            <hr className="w-11/12 mx-auto my-3" />
+          </div>
         </div>
       </div>
     </div>
